416786a218dffe44b4ba523a9ad7415023c781d7
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-08-28 01:54:46 -0300

    Criado truthyfalsy
    Diferenças underfined, null 0 e ' ' == false, 1 == true

---
+++ truthyfalsy.js
@@ -0,0 +1,27 @@
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy 0 => false 1 => true
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true);
+
+//undefined
+
+//null => vazio
+
+let minhaVar;
+let varnull = null;
+
+console.log(minhaVar);
+console.log(varnull);
+
+let numero = 3;
+let texto = 'Daniel';
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varnull);
