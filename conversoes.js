42b0951d1275e9de534a8eb2d4ce272e531fe345
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-08-28 02:24:35 -0300

    Criado arquivo conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dados
+// booleanos
+
+//conversão implicita
+const numero = 527;
+const numeroString = '527';
+
+console.log(numero == numeroString);
+
+// conversão explicita 
+//number()
+//string()
+console.log(numero + Number(numeroString));
