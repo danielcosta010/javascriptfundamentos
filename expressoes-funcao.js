915534f359453b19dde1346709d545b571744705
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-08-29 22:21:50 -0300

    Aula expressoes de função

---
+++ expressoes-funcao.js
@@ -0,0 +1,19 @@
+function minhaFuncao(param) {
+  // bloco de código
+}
+minhaFuncao("param");
+
+// Expressão de função
+
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(2, 3));
+
+// Diferença principal, 
+
+console.log(apresentar());
+
+// Esta forma a função pode ser chamada antes de ser declarada
+
+function apresentar() {
+  return 'Olá';
+}
